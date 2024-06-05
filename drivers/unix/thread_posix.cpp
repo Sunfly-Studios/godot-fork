caa69ed00d905bc8fd1b5b496ae14e2752067a94
debian build <debian@godotbuilds.com>
2024-06-05 14:55:24 +0000

    Make thread_posix compile propertly in older Linuxs
    (cherry picked from commit 6aad26bb7d55d6144d48b32def512f79fa6e1de9)

--- drivers/unix/thread_posix.cpp
+++ drivers/unix/thread_posix.cpp
@@ -39,6 +39,43 @@
 #include <pthread_np.h>
 #endif
 
+#if defined(__GLIBC__) && __GLIBC__ >= 2 && __GLIBC_MINOR__ < 12
+
+#include "core/templates/hash_map.h"
+#include "core/os/mutex.h"
+
+// Make sure that type stays consistent
+// between architectures.
+using ThreadID = uint64_t;
+
+static HashMap<ThreadID, String> thread_names;
+static Mutex thread_names_mutex;
+
+// Make sure that type stays consistent
+// between architectures.
+static ThreadID convert_pthread_to_key(pthread_t thread_id) {
+	return static_cast<ThreadID>(thread_id);
+}
+
+static void set_thread_name(pthread_t thread_id, const String &p_name) {
+    MutexLock lock(thread_names_mutex);
+    ThreadID key = convert_pthread_to_key(thread_id);
+    thread_names[key] = p_name;
+}
+
+static String get_thread_name(pthread_t thread_id) {
+	MutexLock lock(thread_names_mutex);
+	
+	ThreadID key = convert_pthread_to_key(thread_id);
+    HashMap<ThreadID, String>::ConstIterator it = thread_names.find(key);
+    if (it != thread_names.end()) {
+        return it->value;
+    }
+    return String("");
+}
+
+#endif
+
 static Error set_name(const String &p_name) {
 #ifdef PTHREAD_NO_RENAME
 	return ERR_UNAVAILABLE;
@@ -59,7 +96,13 @@ static Error set_name(const String &p_name) {
 #elif defined(PTHREAD_NETBSD_SET_NAME)
 	int err = pthread_setname_np(running_thread, "%s", const_cast<char *>(p_name.utf8().get_data()));
 #else
-	int err = pthread_setname_np(running_thread, p_name.utf8().get_data());
+    #if defined(__GLIBC__) && __GLIBC__ >= 2 && __GLIBC_MINOR__ < 12
+        // Older distros support.
+        set_thread_name(running_thread, p_name.utf8().get_data());
+        int err = 0;
+    #else
+    	int err = pthread_setname_np(running_thread, p_name.utf8().get_data());
+    #endif
 #endif // PTHREAD_BSD_SET_NAME
 
 #endif // PTHREAD_RENAME_SELF
