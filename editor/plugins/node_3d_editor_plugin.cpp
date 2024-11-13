Sunfly-Studios/godot

Fix docs and prefer Colourful icons for 3d node plugin

editor/plugins/node_3d_editor_plugin.cpp
@@ -7798,8 +7798,10 @@ void Node3DEditor::_update_theme() {
 	view_menu->get_popup()->set_item_icon(view_menu->get_popup()->get_item_index(MENU_VIEW_USE_3_VIEWPORTS_ALT), get_editor_theme_icon(SNAME("Panels3Alt")));
 	view_menu->get_popup()->set_item_icon(view_menu->get_popup()->get_item_index(MENU_VIEW_USE_4_VIEWPORTS), get_editor_theme_icon(SNAME("Panels4")));
 
-	sun_button->set_icon(get_editor_theme_icon(SNAME("PreviewSun")));
-	environ_button->set_icon(get_editor_theme_icon(SNAME("PreviewEnvironment")));
+	// I prefer the coloured Sun and Environment
+	// icons than the more-consistent grey ones.
+	sun_button->set_icon(get_editor_theme_icon(SNAME("DirectionalLight3D")));
+	environ_button->set_icon(get_editor_theme_icon(SNAME("WorldEnvironment")));
 	sun_environ_settings->set_icon(get_editor_theme_icon(SNAME("GuiTabMenuHl")));
 
 	sun_title->add_theme_font_override(SceneStringName(font), get_theme_font(SNAME("title_font"), SNAME("Window")));

Sunfly-Studios/godot-fork

(cherry picked from commit 09f3fccc86d866fb2fda2464aa98140d1623a3c3)

editor/plugins/node_3d_editor_plugin.cpp
@@ -8014,8 +8014,10 @@ void Node3DEditor::_update_theme() {
 	view_menu->get_popup()->set_item_icon(view_menu->get_popup()->get_item_index(MENU_VIEW_USE_3_VIEWPORTS_ALT), get_editor_theme_icon(SNAME("Panels3Alt")));
 	view_menu->get_popup()->set_item_icon(view_menu->get_popup()->get_item_index(MENU_VIEW_USE_4_VIEWPORTS), get_editor_theme_icon(SNAME("Panels4")));
 
-	sun_button->set_button_icon(get_editor_theme_icon(SNAME("PreviewSun")));
-	environ_button->set_button_icon(get_editor_theme_icon(SNAME("PreviewEnvironment")));
+	// I prefer the coloured Sun and Environment
+	// icons than the more-consistent grey ones.
+	sun_button->set_button_icon(get_editor_theme_icon(SNAME("DirectionalLight3D")));
+	environ_button->set_button_icon(get_editor_theme_icon(SNAME("WorldEnvironment")));
 	sun_environ_settings->set_button_icon(get_editor_theme_icon(SNAME("GuiTabMenuHl")));
 
 	sun_title->add_theme_font_override(SceneStringName(font), get_theme_font(SNAME("title_font"), SNAME("Window")));