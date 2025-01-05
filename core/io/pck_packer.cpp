sheddy1/Synk

Implement support for PCK signing.

core/io/pck_packer.cpp
@@ -29,6 +29,7 @@
 /**************************************************************************/
 
 #include "pck_packer.h"
+#include "pck_packer.compat.inc"
 
 #include "core/crypto/crypto_core.h"
 #include "core/io/file_access.h"
@@ -48,8 +49,24 @@ static int _get_pad(int p_alignment, int p_n) {
 
 void PCKPacker::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("pck_start", "pck_name", "alignment", "key", "encrypt_directory"), &PCKPacker::pck_start, DEFVAL(32), DEFVAL("0000000000000000000000000000000000000000000000000000000000000000"), DEFVAL(false));
-	ClassDB::bind_method(D_METHOD("add_file", "pck_path", "source_path", "encrypt"), &PCKPacker::add_file, DEFVAL(false));
+	ClassDB::bind_method(D_METHOD("add_file", "pck_path", "source_path", "encrypt", "require_verification"), &PCKPacker::add_file, DEFVAL(false), DEFVAL(false));
 	ClassDB::bind_method(D_METHOD("flush", "verbose"), &PCKPacker::flush, DEFVAL(false));
+	ClassDB::bind_method(D_METHOD("flush_and_sign", "private_key", "curve", "verbose"), &PCKPacker::flush_and_sign, DEFVAL(ECP_DP_SECP256R1), DEFVAL(false));
+
+	BIND_ENUM_CONSTANT(ECP_DP_NONE);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP192R1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP224R1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP256R1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP384R1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP521R1);
+	BIND_ENUM_CONSTANT(ECP_DP_BP256R1);
+	BIND_ENUM_CONSTANT(ECP_DP_BP384R1);
+	BIND_ENUM_CONSTANT(ECP_DP_BP512R1);
+	BIND_ENUM_CONSTANT(ECP_DP_CURVE25519);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP192K1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP224K1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP256K1);
+	BIND_ENUM_CONSTANT(ECP_DP_CURVE448);
 }
 
 Error PCKPacker::pck_start(const String &p_file, int p_alignment, const String &p_key, bool p_encrypt_directory) {
@@ -106,7 +123,7 @@ Error PCKPacker::pck_start(const String &p_file, int p_alignment, const String &
 	return OK;
 }
 
-Error PCKPacker::add_file(const String &p_file, const String &p_src, bool p_encrypt) {
+Error PCKPacker::add_file(const String &p_file, const String &p_src, bool p_encrypt, bool require_verification) {
 	ERR_FAIL_COND_V_MSG(file.is_null(), ERR_INVALID_PARAMETER, "File must be opened before use.");
 
 	Ref<FileAccess> f = FileAccess::open(p_src, FileAccess::READ);
@@ -124,14 +141,14 @@ Error PCKPacker::add_file(const String &p_file, const String &p_src, bool p_encr
 
 	Vector<uint8_t> data = FileAccess::get_file_as_bytes(p_src);
 	{
-		unsigned char hash[16];
-		CryptoCore::md5(data.ptr(), data.size(), hash);
 		pf.md5.resize(16);
-		for (int i = 0; i < 16; i++) {
-			pf.md5.write[i] = hash[i];
-		}
+		CryptoCore::md5(data.ptr(), data.size(), pf.md5.ptrw());
+
+		pf.sha256.resize(32);
+		CryptoCore::sha256(data.ptr(), data.size(), pf.sha256.ptrw());
 	}
 	pf.encrypted = p_encrypt;
+	pf.require_verification = require_verification;
 
 	uint64_t _size = pf.size;
 	if (p_encrypt) { // Add encryption overhead.
@@ -152,12 +169,27 @@ Error PCKPacker::add_file(const String &p_file, const String &p_src, bool p_encr
 }
 
 Error PCKPacker::flush(bool p_verbose) {
+	return flush_and_sign(String(), ECP_DP_NONE, p_verbose);
+}
+
+Error PCKPacker::flush_and_sign(const String &p_private_key, PCKPacker::CurveType p_curve, bool p_verbose) {
 	ERR_FAIL_COND_V_MSG(file.is_null(), ERR_INVALID_PARAMETER, "File must be opened before use.");
+
+#if !defined(ECDSA_ENABLED)
+	ERR_FAIL_COND_V_MSG(!p_private_key.is_empty(), ERR_INVALID_PARAMETER, "Engine was built without ECDSA support.");
+#endif
 
 	int64_t file_base_ofs = file->get_position();
 	file->store_64(0); // files base
 
-	for (int i = 0; i < 16; i++) {
+#if defined(ECDSA_ENABLED)
+	uint64_t signature_info_ofs = file->get_position();
+#endif
+	file->store_64(0); // signature ofs
+	file->store_64(0); // signature size
+	file->store_32(0); // signature curve
+
+	for (int i = 0; i < 11; i++) {
 		file->store_32(0); // reserved
 	}
 
@@ -177,26 +209,66 @@ Error PCKPacker::flush(bool p_verbose) {
 		fhead = fae;
 	}
 
+	Vector<uint8_t> directory_hash;
+	directory_hash.resize(32);
+	CryptoCore::SHA256Context sha_ctx;
+	sha_ctx.start();
+
 	for (int i = 0; i < files.size(); i++) {
+		static uint8_t zero = 0;
 		int string_len = files[i].path.utf8().length();
 		int pad = _get_pad(4, string_len);
 
-		fhead->store_32(string_len + pad);
+		uint32_t full_len = string_len + pad;
+		fhead->store_32(full_len);
+		sha_ctx.update((const unsigned char *)&full_len, 4);
 		fhead->store_buffer((const uint8_t *)files[i].path.utf8().get_data(), string_len);
+		sha_ctx.update((const unsigned char *)files[i].path.utf8().get_data(), string_len);
 		for (int j = 0; j < pad; j++) {
 			fhead->store_8(0);
+			sha_ctx.update((const unsigned char *)&zero, 1);
 		}
 
 		fhead->store_64(files[i].ofs);
+		sha_ctx.update((const unsigned char *)&files[i].ofs, 8);
 		fhead->store_64(files[i].size); // pay attention here, this is where file is
+		sha_ctx.update((const unsigned char *)&files[i].size, 8);
 		fhead->store_buffer(files[i].md5.ptr(), 16); //also save md5 for file
+		sha_ctx.update((const unsigned char *)files[i].md5.ptr(), 16);
+		fhead->store_buffer(files[i].sha256.ptr(), 32); //also save sha256 for file
+		sha_ctx.update((const unsigned char *)files[i].sha256.ptr(), 32);
 
 		uint32_t flags = 0;
 		if (files[i].encrypted) {
 			flags |= PACK_FILE_ENCRYPTED;
 		}
+		if (files[i].require_verification) {
+			flags |= PACK_FILE_REQUIRE_VERIFICATION;
+		}
 		fhead->store_32(flags);
+		sha_ctx.update((const unsigned char *)&flags, 4);
 	}
+	sha_ctx.finish((unsigned char *)directory_hash.ptrw());
+
+#if defined(ECDSA_ENABLED)
+	Vector<uint8_t> signature;
+	size_t signature_size = 4096;
+	if (!p_private_key.is_empty()) {
+		CharString priv_key = p_private_key.ascii();
+
+		size_t priv_key_len = 0;
+		Vector<uint8_t> buf_priv;
+		buf_priv.resize(1024);
+		uint8_t *w = buf_priv.ptrw();
+		ERR_FAIL_COND_V(CryptoCore::b64_decode(&w[0], buf_priv.size(), &priv_key_len, (unsigned char *)priv_key.ptr(), priv_key.length()) != OK, ERR_CANT_CREATE);
+
+		signature.resize(signature_size);
+		CryptoCore::ECDSAContext ecdsa_ctx((CryptoCore::ECDSAContext::CurveType)p_curve);
+		ecdsa_ctx.set_private_key(buf_priv.ptr(), priv_key_len);
+		ERR_FAIL_COND_V_MSG(ecdsa_ctx.sign((const unsigned char *)directory_hash.ptr(), (unsigned char *)signature.ptr(), &signature_size) != OK, ERR_CANT_CREATE, "Pack directory signing failed.");
+		signature.resize(signature_size);
+	}
+#endif
 
 	if (fae.is_valid()) {
 		fhead.unref();
@@ -253,6 +325,25 @@ Error PCKPacker::flush(bool p_verbose) {
 			print_line(vformat("[%d/%d - %d%%] PCKPacker flush: %s -> %s", count, file_num, float(count) / file_num * 100, files[i].src_path, files[i].path));
 		}
 	}
+
+#if defined(ECDSA_ENABLED)
+	if (!p_private_key.is_empty()) {
+		int signature_padding = _get_pad(alignment, file->get_position());
+		for (int i = 0; i < signature_padding; i++) {
+			file->store_8(0);
+		}
+		uint64_t signature_offset = file->get_position();
+		file->store_buffer(signature);
+		uint64_t signature_end = file->get_position();
+
+		// Update signature offset and size.
+		file->seek(signature_info_ofs);
+		file->store_64(signature_offset);
+		file->store_64(signature_size);
+		file->store_32(p_curve);
+		file->seek(signature_end);
+	}
+#endif
 
 	file.unref();
 	memdelete_arr(buf);

Sunfly-Studios/godot-fork

core/io/pck_packer.cpp
@@ -29,6 +29,7 @@
 /**************************************************************************/
 
 #include "pck_packer.h"
+#include "pck_packer.compat.inc"
 
 #include "core/crypto/crypto_core.h"
 #include "core/io/file_access.h"
@@ -47,10 +48,26 @@ static int _get_pad(int p_alignment, int p_n) {
 }
 
 void PCKPacker::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("pck_start", "pck_path", "alignment", "key", "encrypt_directory"), &PCKPacker::pck_start, DEFVAL(32), DEFVAL("0000000000000000000000000000000000000000000000000000000000000000"), DEFVAL(false));
-	ClassDB::bind_method(D_METHOD("add_file", "target_path", "source_path", "encrypt"), &PCKPacker::add_file, DEFVAL(false));
+	ClassDB::bind_method(D_METHOD("pck_start", "pck_name", "alignment", "key", "encrypt_directory"), &PCKPacker::pck_start, DEFVAL(32), DEFVAL("0000000000000000000000000000000000000000000000000000000000000000"), DEFVAL(false));
+	ClassDB::bind_method(D_METHOD("add_file", "pck_path", "source_path", "encrypt", "require_verification"), &PCKPacker::add_file, DEFVAL(false), DEFVAL(false));
 	ClassDB::bind_method(D_METHOD("add_file_removal", "target_path"), &PCKPacker::add_file_removal);
 	ClassDB::bind_method(D_METHOD("flush", "verbose"), &PCKPacker::flush, DEFVAL(false));
+	ClassDB::bind_method(D_METHOD("flush_and_sign", "private_key", "curve", "verbose"), &PCKPacker::flush_and_sign, DEFVAL(ECP_DP_SECP256R1), DEFVAL(false));
+
+	BIND_ENUM_CONSTANT(ECP_DP_NONE);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP192R1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP224R1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP256R1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP384R1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP521R1);
+	BIND_ENUM_CONSTANT(ECP_DP_BP256R1);
+	BIND_ENUM_CONSTANT(ECP_DP_BP384R1);
+	BIND_ENUM_CONSTANT(ECP_DP_BP512R1);
+	BIND_ENUM_CONSTANT(ECP_DP_CURVE25519);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP192K1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP224K1);
+	BIND_ENUM_CONSTANT(ECP_DP_SECP256K1);
+	BIND_ENUM_CONSTANT(ECP_DP_CURVE448);
 }
 
 Error PCKPacker::pck_start(const String &p_pck_path, int p_alignment, const String &p_key, bool p_encrypt_directory) {
@@ -121,12 +138,15 @@ Error PCKPacker::add_file_removal(const String &p_target_path) {
 	pf.md5.resize(16);
 	pf.md5.fill(0);
 
+	pf.sha256.resize(32);
+	pf.sha256.fill(0);
+
 	files.push_back(pf);
 
 	return OK;
 }
 
-Error PCKPacker::add_file(const String &p_target_path, const String &p_source_path, bool p_encrypt) {
+Error PCKPacker::add_file(const String &p_target_path, const String &p_source_path, bool p_encrypt, bool p_require_verification) {
 	ERR_FAIL_COND_V_MSG(file.is_null(), ERR_INVALID_PARAMETER, "File must be opened before use.");
 
 	Ref<FileAccess> f = FileAccess::open(p_source_path, FileAccess::READ);
@@ -144,14 +164,14 @@ Error PCKPacker::add_file(const String &p_target_path, const String &p_source_pa
 
 	Vector<uint8_t> data = FileAccess::get_file_as_bytes(p_source_path);
 	{
-		unsigned char hash[16];
-		CryptoCore::md5(data.ptr(), data.size(), hash);
 		pf.md5.resize(16);
-		for (int i = 0; i < 16; i++) {
-			pf.md5.write[i] = hash[i];
-		}
+		CryptoCore::md5(data.ptr(), data.size(), pf.md5.ptrw());
+
+		pf.sha256.resize(32);
+		CryptoCore::sha256(data.ptr(), data.size(), pf.sha256.ptrw());
 	}
 	pf.encrypted = p_encrypt;
+	pf.require_verification = p_require_verification;
 
 	uint64_t _size = pf.size;
 	if (p_encrypt) { // Add encryption overhead.
@@ -172,12 +192,27 @@ Error PCKPacker::add_file(const String &p_target_path, const String &p_source_pa
 }
 
 Error PCKPacker::flush(bool p_verbose) {
+	return flush_and_sign(String(), ECP_DP_NONE, p_verbose);
+}
+
+Error PCKPacker::flush_and_sign(const String &p_private_key, PCKPacker::CurveType p_curve, bool p_verbose) {
 	ERR_FAIL_COND_V_MSG(file.is_null(), ERR_INVALID_PARAMETER, "File must be opened before use.");
+
+#if !defined(ECDSA_ENABLED)
+	ERR_FAIL_COND_V_MSG(!p_private_key.is_empty(), ERR_INVALID_PARAMETER, "Engine was built without ECDSA support.");
+#endif
 
 	int64_t file_base_ofs = file->get_position();
 	file->store_64(0); // files base
 
-	for (int i = 0; i < 16; i++) {
+#if defined(ECDSA_ENABLED)
+	uint64_t signature_info_ofs = file->get_position();
+#endif
+	file->store_64(0); // signature ofs
+	file->store_64(0); // signature size
+	file->store_32(0); // signature curve
+
+	for (int i = 0; i < 11; i++) {
 		file->store_32(0); // reserved
 	}
 
@@ -197,19 +232,35 @@ Error PCKPacker::flush(bool p_verbose) {
 		fhead = fae;
 	}
 
+	Vector<uint8_t> directory_hash;
+	directory_hash.resize(32);
+	CryptoCore::SHA256Context sha_ctx;
+	sha_ctx.start();
+
 	for (int i = 0; i < files.size(); i++) {
+		static uint8_t zero = 0;
 		int string_len = files[i].path.utf8().length();
 		int pad = _get_pad(4, string_len);
 
-		fhead->store_32(uint32_t(string_len + pad));
+		uint32_t full_len = string_len + pad;
+		fhead->store_32(full_len);
+		sha_ctx.update((const unsigned char *)&full_len, 4);
+		
 		fhead->store_buffer((const uint8_t *)files[i].path.utf8().get_data(), string_len);
+		sha_ctx.update((const unsigned char *)files[i].path.utf8().get_data(), string_len);
 		for (int j = 0; j < pad; j++) {
 			fhead->store_8(0);
+			sha_ctx.update((const unsigned char *)&zero, 1);
 		}
 
 		fhead->store_64(files[i].ofs);
+		sha_ctx.update((const unsigned char *)&files[i].ofs, 8);
 		fhead->store_64(files[i].size); // pay attention here, this is where file is
+		sha_ctx.update((const unsigned char *)&files[i].size, 8);
 		fhead->store_buffer(files[i].md5.ptr(), 16); //also save md5 for file
+		sha_ctx.update((const unsigned char *)files[i].md5.ptr(), 16);
+		fhead->store_buffer(files[i].sha256.ptr(), 32); //also save sha256 for file
+		sha_ctx.update((const unsigned char *)files[i].sha256.ptr(), 32);
 
 		uint32_t flags = 0;
 		if (files[i].encrypted) {
@@ -218,8 +269,33 @@ Error PCKPacker::flush(bool p_verbose) {
 		if (files[i].removal) {
 			flags |= PACK_FILE_REMOVAL;
 		}
+		if (files[i].require_verification) {
+			flags |= PACK_FILE_REQUIRE_VERIFICATION;
+		}
 		fhead->store_32(flags);
+		sha_ctx.update((const unsigned char *)&flags, 4);
 	}
+	sha_ctx.finish((unsigned char *)directory_hash.ptrw());
+
+#if defined(ECDSA_ENABLED)
+	Vector<uint8_t> signature;
+	size_t signature_size = 4096;
+	if (!p_private_key.is_empty()) {
+		CharString priv_key = p_private_key.ascii();
+
+		size_t priv_key_len = 0;
+		Vector<uint8_t> buf_priv;
+		buf_priv.resize(1024);
+		uint8_t *w = buf_priv.ptrw();
+		ERR_FAIL_COND_V(CryptoCore::b64_decode(&w[0], buf_priv.size(), &priv_key_len, (unsigned char *)priv_key.ptr(), priv_key.length()) != OK, ERR_CANT_CREATE);
+
+		signature.resize(signature_size);
+		CryptoCore::ECDSAContext ecdsa_ctx((CryptoCore::ECDSAContext::CurveType)p_curve);
+		ecdsa_ctx.set_private_key(buf_priv.ptr(), priv_key_len);
+		ERR_FAIL_COND_V_MSG(ecdsa_ctx.sign((const unsigned char *)directory_hash.ptr(), (unsigned char *)signature.ptr(), &signature_size) != OK, ERR_CANT_CREATE, "Pack directory signing failed.");
+		signature.resize(signature_size);
+	}
+#endif
 
 	if (fae.is_valid()) {
 		fhead.unref();
@@ -280,6 +356,25 @@ Error PCKPacker::flush(bool p_verbose) {
 			print_line(vformat("[%d/%d - %d%%] PCKPacker flush: %s -> %s", count, file_num, float(count) / file_num * 100, files[i].src_path, files[i].path));
 		}
 	}
+
+#if defined(ECDSA_ENABLED)
+	if (!p_private_key.is_empty()) {
+		int signature_padding = _get_pad(alignment, file->get_position());
+		for (int i = 0; i < signature_padding; i++) {
+			file->store_8(0);
+		}
+		uint64_t signature_offset = file->get_position();
+		file->store_buffer(signature);
+		uint64_t signature_end = file->get_position();
+
+		// Update signature offset and size.
+		file->seek(signature_info_ofs);
+		file->store_64(signature_offset);
+		file->store_64(signature_size);
+		file->store_32(p_curve);
+		file->seek(signature_end);
+	}
+#endif
 
 	file.unref();
 	memdelete_arr(buf);